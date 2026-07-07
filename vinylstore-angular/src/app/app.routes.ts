f97ebcc67e5d3dcfebbd6d60663b78102da1ccd5
IanHazecure <136367549+IanHazecure@users.noreply.github.com>
2026-07-07 03:29:12 -0400

    fix intro slide

--- vinylstore-angular/src/app/app.routes.ts
+++ vinylstore-angular/src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
 import { Intro } from './pages/intro/intro';
-import { Rock } from './pages/categories/rock/rock';
-import { Jazz } from './pages/categories/jazz/jazz';
-import { Pop } from './pages/categories/pop/pop';
-import { Punk } from './pages/categories/punk/punk';
 import { Login } from './pages/auth/login/login';
 import { Recovery } from './pages/auth/recovery/recovery';
 import { Register } from './pages/auth/register/register';
@@ -14,10 +10,6 @@ import { Catalog } from './pages/catalog/catalog';
 
 export const routes: Routes = [
   { path: '', component: Intro },
-  { path: 'categories/rock', component: Rock },
-  { path: 'categories/jazz', component: Jazz },
-  { path: 'categories/pop', component: Pop },
-  { path: 'categories/punk', component: Punk },
   { path: 'login', component: Login },
   { path: 'recovery', component: Recovery },
   { path: 'register', component: Register },
